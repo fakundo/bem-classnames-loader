eceb2ce8fd8c23127ee61d2567bebe6c6bef3d24
fakundo <fakundo.roman@gmail.com>
2016-11-25 16:43:27 +0300

    Query options support

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,23 +1,20 @@
 var path = require('path');
-var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var bemClassnames = path.join(__dirname, '/../lib/index.js');
-
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index'),
+  entry: path.resolve(__dirname, 'src', 'index'),
   devtool: 'source-map',
   output: {
     filename: 'index.js',
-    path: path.join(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
     publicPath: ''
   },
   plugins: [
     new ExtractTextPlugin('index.css'),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'index.html'),
+      template: path.resolve(__dirname, 'index.html'),
       filename: 'index.html'
     })
   ],
@@ -26,7 +23,11 @@ module.exports = {
       {
         test: /\.scss$/,
         //loader: bemClassnames + '!style!css!sass'
-        loaders: [bemClassnames, ExtractTextPlugin.extract('css!sass')]
+        loaders: [
+          path.resolve(__dirname, '../lib/index.js'),
+          ExtractTextPlugin.extract('css!sass')
+        ],
+        //query: { bemClassnames: { prefixes: { state: 'IS-' } } }
       }
     ]
   }
